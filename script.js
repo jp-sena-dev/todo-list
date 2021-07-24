d6ed8eb4b3ab539339c721d17d4895bc7798d382
João Pedro Sena <joaopedrosena1997@gmail.com>
2021-07-24 00:43:09 -0300

    Deletar item

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-const elementosSalvo = () => {
-  const salvos = localStorage;
-  for (let index = 0; index <= salvos.length; index += 1) {
-    const elemento = salvos.getItem(index);
+const mostrandoElementosSalvo = () => {
+  for (let index = 0; index <= localStorage.length; index += 1) {
+    let elemento = localStorage[index];
     if (elemento != undefined){
       criadorTarefa(elemento);
-    }
-  }
+    };
+  };
 };
 
 const salvarInformacao = () => {
+  localStorage.clear();
   const tarefas = document.querySelectorAll('.conteudoTarefa');
-  let numeroTarefa = 0;
-  tarefas.forEach((tarefa) => {
-    numeroTarefa += 1;
-    localStorage.setItem(numeroTarefa, tarefa.innerHTML);
+  // localStorage.setItem('tarefas', JSON.stringify([1, 2, 3, 4]));
+  tarefas.forEach((tarefa, index) => {
+    localStorage.setItem(index, tarefa.innerHTML);
   });
 };
 
 const verificadorInput = () => {
   const tarefa = document.querySelector('#tarefaConteudo');
-  tarefa.addEventListener("keyup", (key) => {
-    if (key.keyCode === 13)  {
+  tarefa.addEventListener("keyup", (tecla) => {
+    if (tecla.key === 'Enter')  {
       if (tarefa.value.length != 0) {
         criadorTarefa(tarefa.value);
         salvarInformacao();
@@ -32,23 +31,44 @@ const verificadorInput = () => {
   });
 };
 
+const clickDeletar = (tarefa, icone, conteudoHtml) => {
+  icone.addEventListener("click", () => {
+    tarefa.classList.add('deletado');
+    setTimeout(() => {
+      const conteudo = conteudoHtml.innerHTML;
+      console.log(tarefa)
+      tarefa.remove();
+      salvarInformacao();
+    }, 500);
+  });
+};
+
 const criadorTarefa = (conteudo) => {
   const tarefas = document.querySelector('.tarefas');
   const tarefa = document.createElement('li');
   const conteudoTarefa = document.createElement('p');
   const icones = document.createElement('div');
+  const iconeLixiera = document.createElement('i');
+  const iconeEditar = document.createElement('i'); 
 
   tarefa.classList.add('tarefa');
   conteudoTarefa.classList.add('conteudoTarefa');
+  iconeLixiera.classList.add('icon-delete');
+  iconeEditar.classList.add('icon-edit');
 
   conteudoTarefa.innerHTML = conteudo;
 
   tarefas.appendChild(tarefa);
   tarefa.appendChild(conteudoTarefa);
+  tarefa.appendChild(icones);
+  icones.appendChild(iconeEditar);
+  icones.appendChild(iconeLixiera);
 
+  clickDeletar(tarefa, iconeLixiera, conteudoTarefa);
+  // adicionarVerificadorClick(iconeEditar);
 };
 
 window.onload = () => {
-  elementosSalvo();
+  mostrandoElementosSalvo();
   verificadorInput();
 };
