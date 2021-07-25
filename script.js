bc3b0d540cf023d6431fbd0ac81c21936ed1a5ce
João Pedro Sena <joaopedrosena1997@gmail.com>
2021-07-25 12:47:38 -0300

    Editar

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
+/* mostrando elementos do localStorage */
 const mostrandoElementosSalvo = () => {
   for (let index = 0; index <= localStorage.length; index += 1) {
-    let elemento = localStorage[index];
-    if (elemento != undefined){
-      criadorTarefa(elemento);
+    if (localStorage[index] != undefined){
+      criadorTarefa(localStorage[index]);
     };
   };
 };
 
+/* salvando informações no localStorage */
 const salvarInformacao = () => {
   localStorage.clear();
-  const tarefas = document.querySelectorAll('.conteudoTarefa');
+  const tarefas = document.querySelectorAll(".conteudoTarefa");
   // localStorage.setItem('tarefas', JSON.stringify([1, 2, 3, 4]));
   tarefas.forEach((tarefa, index) => {
     localStorage.setItem(index, tarefa.innerHTML);
   });
 };
 
+/* quando apertar a tecla enter */
 const verificadorInput = () => {
-  const tarefa = document.querySelector('#tarefaConteudo');
+  const tarefa = document.querySelector("#tarefaConteudo");
   tarefa.addEventListener("keyup", (tecla) => {
     if (tecla.key === 'Enter')  {
       if (tarefa.value.length != 0) {
@@ -25,47 +27,84 @@ const verificadorInput = () => {
         salvarInformacao();
         tarefa.value = "";
       } else {
-        alert("Digite uma tarefa");
+        alert('Digite uma tarefa');
       };
     };
   });
 };
 
-const clickDeletar = (tarefa, icone, conteudoHtml) => {
+/* deletando tarefa */
+const clickDeletar = (tarefa, icone) => {
   icone.addEventListener("click", () => {
-    tarefa.classList.add('deletado');
+    tarefa.classList.add("deletado");
     setTimeout(() => {
-      const conteudo = conteudoHtml.innerHTML;
-      console.log(tarefa)
       tarefa.remove();
       salvarInformacao();
-    }, 500);
+    }, 300);
   });
 };
 
-const criadorTarefa = (conteudo) => {
-  const tarefas = document.querySelector('.tarefas');
-  const tarefa = document.createElement('li');
-  const conteudoTarefa = document.createElement('p');
-  const icones = document.createElement('div');
-  const iconeLixiera = document.createElement('i');
-  const iconeEditar = document.createElement('i'); 
+/* editar */
+const clickEditar = (tarefa, icone) => {
+  icone.addEventListener("click", () => {
+    const quantidadeEditor = document.querySelectorAll(".editando");
+    if (quantidadeEditor.length != 1) {
+      const input = document.createElement("input");
+      const iconeEnter = document.createElement("i");
+      const conteudoTarefa = tarefa.children[0];
 
-  tarefa.classList.add('tarefa');
-  conteudoTarefa.classList.add('conteudoTarefa');
-  iconeLixiera.classList.add('icon-delete');
-  iconeEditar.classList.add('icon-edit');
+      input.classList.add("editando");
+  
+      input.value = conteudoTarefa.innerHTML;
+      tarefa.insertBefore(input, conteudoTarefa);
+      input.focus();
+
+      conteudoTarefa.remove();
+      opcoesEditar(tarefa, input);
+    };
+  });
+};
+
+const opcoesEditar = (tarefa, input) => {
+  const conteudo = document.createElement("p");
+  input.addEventListener("blur", () => {
+    conteudo.innerHTML = input.value;
+    tarefa.insertBefore(tarefa.appendChild(conteudo), input);
+    input.remove();
+    conteudo.classList.add("conteudoTarefa");
+    salvarInformacao();
+  });
+  input.addEventListener("keyup", (tecla) => {
+    if (tecla.key === 'Enter') {
+      input.blur();
+    };
+  });
+};
+
+/* mostrar tarefa na tela com todos os elementos */
+const criadorTarefa = (conteudo) => {
+  const tarefas = document.querySelector(".tarefas");
+  const tarefa = document.createElement("li");
+  const conteudoTarefa = document.createElement("p");
+  const icones = document.createElement("div");
+  const iconeLixiera = document.createElement("i");
+  const iconeEditar = document.createElement("i"); 
+
+  tarefa.classList.add("tarefa");
+  conteudoTarefa.classList.add("conteudoTarefa");
+  iconeLixiera.classList.add("icon-delete");
+  iconeEditar.classList.add("icon-edit");
 
   conteudoTarefa.innerHTML = conteudo;
+
+  clickDeletar(tarefa, iconeLixiera);
+  clickEditar(tarefa, iconeEditar);
 
   tarefas.appendChild(tarefa);
   tarefa.appendChild(conteudoTarefa);
   tarefa.appendChild(icones);
   icones.appendChild(iconeEditar);
   icones.appendChild(iconeLixiera);
-
-  clickDeletar(tarefa, iconeLixiera, conteudoTarefa);
-  // adicionarVerificadorClick(iconeEditar);
 };
 
 window.onload = () => {
